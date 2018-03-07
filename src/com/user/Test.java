9af3495b8960c412e5964e4ae84f4b477deb0487
gdutLiang <1512160785@qq.com>
2018-03-07 16:21:04 +0800

    mybatis v1.0

---
+++ src/com/user/Test.java
@@ -0,0 +1,23 @@
+package com.user;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatisConfig.xml";
+        InputStream res = Resources.getResourceAsStream(resource);
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(res);
+        SqlSession session = factory.openSession();
+        String statement = "com.user.UserMapper.getUserById";
+        User user = session.selectOne(statement, 1);
+        System.out.println(user.toString());
+    }
+}
